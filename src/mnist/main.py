f3995ad6aec1d669cb75c67d141b4c7693165c3a
hmatzner <hernoo.m@gmail.com>
2024-06-25 21:56:03 -0300

    changed structure, started mnist

---
+++ src/mnist/main.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+transform = transforms.ToTensor()
+
+train_data = datasets.MNIST(root='cnn_data',
+                            train=True,
+                            download=True,
+                            transform=transform)
+
+test_data = datasets.MNIST(root='cnn_data',
+                           train=False,
+                           download=True,
+                           transform=transform)
+
+print(test_data)
+
+
+class ConvolutionalNetwork(nn.Module):
+    pass
