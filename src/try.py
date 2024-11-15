4cf3c432208502a352240e0feca1cfd48b5040ae
hmatzner <hernoo.m@gmail.com>
2024-11-15 23:37:19 +0200

    play with np and pd

--- src/try.py
+++ src/try.py
@@ -1,6 +1,16 @@
-def trying():
-	pass
+import numpy as np
+import pandas as pd
+
+
+def trying(number):
+	number_exp = np.array([[1, 2, 3], [4, 5, 6]])
+	df = pd.DataFrame(number_exp * number)
+	return df
+
+
+def main():
+	trying(3)
 
 
 if __name__ == '__main__':
-	trying()
+	main()
