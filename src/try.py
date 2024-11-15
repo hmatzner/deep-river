ae43018d9e73fe28919cff1792897f816969d8b1
hmatzner <hernoo.m@gmail.com>
2024-11-15 23:28:49 +0200

    [add] first dummy function

--- src/try.py
+++ src/try.py
@@ -1,2 +1,14 @@
-def trying():
-	pass
+import time
+
+
+def trying(number):
+	for i in range(int(number)):
+		time.sleep(i)
+		if i == 0:
+			print(f'Good, you waited for {i} seconds.')
+		else:
+			print(f'Good, you waited for an extra {i} seconds')
+
+
+def main():
+	trying(4)
